d114718bc336134b1019838d62ef5611a415a2bc
alexmor24 <morquecho94a@gmal.com>
2026-01-11 12:14:32 -0600

    Add Test Cases

---
+++ tests/unit/NotchAttenuationTest.m
@@ -0,0 +1,12 @@
+classdef NotchAttenuationTest < sltest.TestCase
+
+   methods (Test)
+       function test1(testCase)
+%           model = 'vdp';
+%           testCase.loadSystem(model);
+%           simOut = testCase.simulate(model);
+%           testCase.verifySignalsMatch(simOut,'baseline_vdp.mat');
+       end
+   end
+   
+end
